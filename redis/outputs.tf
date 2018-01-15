080a615beb1c8e9db173708e37eca5b9889e2b1c
Luca <luca@skyscrapers.eu>
2018-01-15 18:36:03 +0100

    fixing unexisting output in redis module

--- redis/outputs.tf
+++ redis/outputs.tf
@@ -2,10 +2,6 @@ output "redis_sg" {
   value = "${aws_security_group.sg_redis.id}"
 }
 
-output "configuration_endpoint_address" {
-  value = "${aws_elasticache_replication_group.redis.configuration_endpoint_address}"
-}
-
 output "primary_endpoint_address" {
   value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
 }
