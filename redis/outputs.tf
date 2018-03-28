3338aaeaa14f2a7eb2ae63709b58118b7d7cb8fc
Philip Dubois <hello@philipdubois.be>
2018-03-28 12:11:46 +0200

    Deal with empty auth_token

--- redis/outputs.tf
+++ redis/outputs.tf
@@ -3,5 +3,5 @@ output "redis_sg" {
 }
 
 output "primary_endpoint_address" {
-  value = "${aws_elasticache_replication_group.redis.primary_endpoint_address}"
+  value = "${element(concat(aws_elasticache_replication_group.redis.*.primary_endpoint_address, aws_elasticache_replication_group.redis_auth.*.primary_endpoint_address), 0)}"
 }
