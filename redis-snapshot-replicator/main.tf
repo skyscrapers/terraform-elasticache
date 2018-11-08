98ffac7ed6f24f1757144255fb9e957264a8f60e
Wouter Reyntjens <wouter@skyscrapers.eu>
2018-11-08 14:45:08 +0100

    Change naming convention and add removal script

--- redis-snapshot-replicator/main.tf
+++ redis-snapshot-replicator/main.tf
@@ -1,5 +1,5 @@
-resource "aws_iam_role" "replication-role" {
-  name = "${var.name}-replication-role"
+resource "aws_iam_role" "replication_role" {
+  name = "${var.name}-replication-role-${var.environment}"
 
   assume_role_policy = <<POLICY
 {
@@ -57,18 +57,18 @@ data "aws_iam_policy_document" "replication_policy" {
   }
 }
 
-resource "aws_iam_policy" "replication-policy" {
-  name = "${var.name}-replication-policy"
+resource "aws_iam_policy" "replication_policy" {
+  name = "${var.name}-replication-policy-${var.environment}"
   policy = "${data.aws_iam_policy_document.replication_policy.json}"
 }
 
-resource "aws_iam_role_policy_attachment" "replication-replication-attachement" {
-  role       = "${aws_iam_role.replication-role.name}"
-  policy_arn = "${aws_iam_policy.replication-policy.arn}"
+resource "aws_iam_role_policy_attachment" "replication_replication_attachement" {
+  role       = "${aws_iam_role.replication_role.name}"
+  policy_arn = "${aws_iam_policy.replication_policy.arn}"
 } 
 
 resource "aws_s3_bucket" "destination" {
-  bucket   = "${var.name}-snapshots-destination"
+  bucket   = "${var.name}-snapshots-destination-${var.environment}"
   provider = "aws.replica"
 
   versioning {
@@ -78,14 +78,14 @@ resource "aws_s3_bucket" "destination" {
 
 resource "aws_s3_bucket" "bucket" {
   provider = "aws"
-  bucket   = "${var.name}-snapshots"
+  bucket   = "${var.name}-snapshots-${var.environment}"
 
   versioning {
     enabled = true
   }
 
   replication_configuration {
-    role = "${aws_iam_role.replication-role.arn}"
+    role = "${aws_iam_role.replication_role.arn}"
 
     rules {
       id     = "redis"
@@ -101,7 +101,7 @@ resource "aws_s3_bucket" "bucket" {
 }
 
 resource "aws_iam_policy" "redis_lambda_create_snapshot" {
-  name = "${var.name}_lambda_create_snapshot"
+  name = "${var.name}-lambda-create-snapshot-${var.environment}"
 
   policy = <<EOF
 {
@@ -118,7 +118,7 @@ EOF
 }
 
 resource "aws_iam_role" "iam_for_lambda_redis" {
-  name = "default_lambda_for_${var.name}"
+  name = "default-lambda-for-${var.name}-${var.environment}"
 
   assume_role_policy = <<EOF
 {
@@ -142,7 +142,7 @@ resource "aws_iam_role_policy_attachment" "attach_lambda_create_policy_to_role" 
 }
 
 resource "aws_iam_policy" "redis_lambda_copy" {
-  name = "${var.name}_lambda_copy"
+  name = "${var.name}-lambda-copy-${var.environment}"
 
   policy = <<EOF
 {
@@ -171,11 +171,33 @@ resource "aws_iam_role_policy_attachment" "attach_redis_lambda_copy_policy_to_ro
   policy_arn = "${aws_iam_policy.redis_lambda_copy.arn}"
 }
 
+resource "aws_iam_policy" "redis_lambda_remove" {
+  name = "${var.name}-lambda-remove-${var.environment}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [{
+    "Effect": "Allow",
+    "Action": [
+        "elasticache:DescribeSnapshots",
+        "elasticache:DeleteSnapshot"
+    ],
+    "Resource": "*"
+  }]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "attach_redis_lambda_remove_policy_to_role" {
+  role       = "${aws_iam_role.iam_for_lambda_redis.name}"
+  policy_arn = "${aws_iam_policy.redis_lambda_remove.arn}"
+}
+
 #Create zip file with all lambda code
 data "archive_file" "create_zip" {
   type        = "zip"
   output_path = "${path.module}/shipper.zip"
-
   source_dir = "${path.module}/functions/"
 }
 
@@ -190,7 +212,7 @@ locals {
 #Creation of lambda function to create snapshots
 
 resource "aws_lambda_function" "redis_create_snapshot" {
-  function_name = "${var.name}_create_snapshot"
+  function_name = "${var.name}-create-snapshot-${var.environment}"
   role          = "${aws_iam_role.iam_for_lambda_redis.arn}"
   handler       = "create_snapshot.lambda_handler"
 
@@ -210,7 +232,7 @@ resource "aws_lambda_function" "redis_create_snapshot" {
 #Creation of lambda function to copy snapshots
 
 resource "aws_lambda_function" "redis_copy_snapshot" {
-  function_name = "${var.name}_copy_snapshot"
+  function_name = "${var.name}-copy-snapshot-${var.environment}"
   role          = "${aws_iam_role.iam_for_lambda_redis.arn}"
   handler       = "shipper.lambda_handler"
 
@@ -228,31 +250,62 @@ resource "aws_lambda_function" "redis_copy_snapshot" {
   }
 }
 
+#Creation of lambda function to remove source snapshots
+
+resource "aws_lambda_function" "redis_remove_snapshot" {
+  function_name = "${var.name}-remove-snapshot-${var.environment}"
+  role          = "${aws_iam_role.iam_for_lambda_redis.arn}"
+  handler       = "shipper.lambda_handler"
+
+  filename         = "${local.filename}"
+  source_code_hash = "${base64sha256(file("${local.filename}"))}"
+
+  runtime = "python2.7"
+  timeout = "120"
+
+  environment {
+    variables = {
+      DB_INSTANCES  = "${join(",",var.db_instances)}"
+    }
+  }
+}
+
 #Creation of cronjobs 
 #Job for triggering backups
 resource "aws_cloudwatch_event_rule" "redis_every_6_hours" {
-  name                = "${var.name}_every_6_hours"
+  name                = "${var.name}-every-6_hours-${var.environment}"
   description         = "Fires every 6 hours"
   schedule_expression = "rate(6 hours)"
 }
 
 resource "aws_cloudwatch_event_target" "activate_create_redis_backup_cron" {
   rule      = "${aws_cloudwatch_event_rule.redis_every_6_hours.name}"
-  target_id = "${var.name}-snapshot-creation"
+  target_id = "${var.name}-snapshot-creation-${var.environment}"
   arn       = "${aws_lambda_function.redis_create_snapshot.arn}"
 }
 
 #Job for triggering copy job
 resource "aws_cloudwatch_event_rule" "redis_every_7_hours" {
-  name                = "${var.name}_every_7_hours"
+  name                = "${var.name}-every-7-hours-${var.environment}"
   description         = "Fires every 7 hours"
   schedule_expression = "rate(7 hours)"
 }
 
 resource "aws_cloudwatch_event_target" "activate_copy_redis_snapshot_cron" {
   rule      = "${aws_cloudwatch_event_rule.redis_every_7_hours.name}"
-  target_id = "${var.name}-snapshot-copy"
+  target_id = "${var.name}-snapshot-copy-${var.environment}"
   arn       = "${aws_lambda_function.redis_copy_snapshot.arn}"
+}
+resource "aws_cloudwatch_event_rule" "redis_every_8_hours" {
+  name                = "${var.name}-every-8-hours-${var.environment}"
+  description         = "Fires every 8 hours"
+  schedule_expression = "rate(8 hours)"
+}
+
+resource "aws_cloudwatch_event_target" "activate_remove_redis_snapshot_cron" {
+  rule      = "${aws_cloudwatch_event_rule.redis_every_8_hours.name}"
+  target_id = "${var.name}-snapshot-remove-${var.environment}"
+  arn       = "${aws_lambda_function.redis_remove_snapshot.arn}"
 }
 
 #Allow cloudwatch to execute lambda
@@ -272,6 +325,10 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_create" {
   source_arn    = "${aws_cloudwatch_event_rule.redis_every_6_hours.arn}"
 }
 
-
-
-
+resource "aws_lambda_permission" "allow_cloudwatch_to_call_lambda_remove" {
+  statement_id  = "AllowExecutionFromCloudWatch"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.redis_remove_snapshot.arn}"
+  principal     = "events.amazonaws.com"
+  source_arn    = "${aws_cloudwatch_event_rule.redis_every_8_hours.arn}"
+}
