1bec3a2122c32e431a5c10e7a4c2775e33ea9660
iuri aranda <iuri.aranda@gmail.com>
2023-06-05 12:04:50 +0200

    Add aws provider version constraints
    V5.x is not yet supported

--- redis/versions.tf
+++ redis/versions.tf
@@ -1,4 +1,13 @@
 
 terraform {
   required_version = ">= 0.12"
+
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # Version 5.x introduces some breaking changes for aws_elasticache_* resources
+      # and this module does not support it
+      version = "< 5.0.0"
+    }
+  }
 }
